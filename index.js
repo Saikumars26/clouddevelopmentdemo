89d61c0536bea589338ceda7642f97d8248a1e94
Saikumars26 <saikumarsheri01@gmail.com>
2025-04-11 00:02:03 -0500

    added file

--- index.js
+++ index.js
@@ -9,9 +9,9 @@ app.use(express.json());
 // MySQL connection config
 // triggering azure deployment
 const db = mysql.createConnection({
-  host: 'studentdb-demo.mysql.database.azure.com',
-  user: 'mysql_admin',
-  password: 'TempPass@123',
+  host: 'saisheri.mysql.database.azure.com',
+  user: 'Saisherii',
+  password: 'Sai@0226',
   database: 'studentdb',
   port: 3306,
   ssl: {
